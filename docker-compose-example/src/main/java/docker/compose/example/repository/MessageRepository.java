0188f32548d13100c6411870bda60486f8185684
Marco Pavon <39383149+marcckku@users.noreply.github.com>
2024-04-02 22:50:34 +0200

    Delete docker-compose-example directory
    Signed-off-by: Marco Pavon <39383149+marcckku@users.noreply.github.com>

--- docker-compose-example/src/main/java/docker/compose/example/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package docker.compose.example.repository;
-
-import docker.compose.example.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
